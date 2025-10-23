d2af89a369bcd1e5d6026594266c000ae8c580dc
JhohanBustamante <jhohantma@gmail.com>
2025-10-23 16:54:04 +0000

    Actualización desde debian

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
+
 var config = {};
 
-config.bd = "BackendVenta";
+config.bd = "admin";
 config.port = 3000;
 config.claveSecreta = "123abc098zyx";
-config.dominio = "https://bit.jhohanbustamante.online"
+config.dominio = "https://ventarb.kolisevm.online"
 config.expiracion = 1000 * 60 * 60
 
-config.bdUser = "adminVenta"
+config.bdUser = "rootAdmin"
 config.bdPass = "prueba123"
-config.bdIp = "165.227.2.200"
+config.bdIp = "64.227.58.82"
 config.bdPort = "27017"
 config.produccion = true
 
@@ -23,7 +24,9 @@ config.negocio = "Venta de Lotes RB"
 config.listaCors = [
     "http://localhost:4200",
     "http://localhost:3000",
-    "https://bit.jhohanbustamante.online"
+    "https://ventarb.kolisevm.online",
+"http://64.227.58.82:3000",
+"https://64.227.58.82:3000"
 ]
 
 module.exports.config = config;
